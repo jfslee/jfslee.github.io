9f945b6ebd3c42fed9d5cac74da2b4bc502d4ffc
Josh <74288129+jfslee@users.noreply.github.com>
2026-02-27 11:28:02 +1100

    remove NF dependencies; small layout/JS tweaks

--- base.js
+++ base.js
@@ -1,21 +1,26 @@
 function k(id){var e=document.getElementById(id);e.style.display='none';}
 function s(id){var e=document.getElementById(id);e.style.display='block';}
 function phase(id){var e=document.getElementById(id);if(e.style.display=='none'){s(id);}else{k(id);}}
-function cc(n,x){n = document.getElementById(n); if (n.nodeName == 'svg') {n.style.fill=x} else {n.style.color=x}}
+
+function cc(n,x){n = document.getElementById(n);
+  if (n.nodeName == 'svg') {n.style.fill=x}
+  else {n.style.color=x;
+    if (n.getElementsByTagName('svg').length==1) {n.getElementsByTagName('svg')[0].style.fill=x}}}
 
 function ch(h){h=Math.trunc(h)%1530;if(h<0){return 1530-Math.abs(h)}else{return(h)}}
 function sh(h){return(Math.max(0,h-510)-Math.max(0,h-765)-Math.max(0,h-1275));}
 function hc(h){return new Array(3).fill().map((x,i)=>sh(ch(h+(510*i))))}
 function hue(h){return "rgb("+hc(h).join(",")+")"}
 
-function hue_ui(d,ids,t){
+function ui(i,x,a) {var e = document.getElementById(i); typeof(a)!='undefined'?e.setAttribute(a,x):e.innerHTML=x}
+function ui_hue(d,ids,t){
   if (typeof ids=='string') {ids=[ids]}
   var h=d.getTime()/t; var l=ids.length;
   ids.forEach((n,i) => cc(n,hue(h-(i/l*1530))))}
 
 function main(){
   const d = new Date();
-  hue_ui(d,['pp','m4a','m4b','m4c','m4d'],137)
+  ui_hue(d,['pp','m4a','m4b','m4c','m4d'],137)
   requestAnimationFrame(main)}
 
 function rs() {
@@ -25,6 +30,6 @@ function rs() {
   document.body.style.zoom = Math.min(w/pw,h/ph);
 }
 
-var d=new Date();document.getElementById('year').innerHTML=d.getFullYear();
-var e=document.getElementById("em");e.setAttribute("href", "mailto:"+"me"+"@"+"joshl.ee");
+ui('year',new Date().getFullYear());
+ui('em',"mailto:"+"me"+"@"+"joshl.ee",'href');
 rs();main();
