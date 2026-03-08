0632bc91ba2cb954982d323046b25a4065120c52
Josh <74288129+jfslee@users.noreply.github.com>
2026-03-08 15:22:18 +1100

    start groundwork for new background switcher

--- base.js
+++ base.js
@@ -15,27 +15,28 @@ function sh(h){return(Math.max(0,h-510)-Math.max(0,h-765)-Math.max(0,h-1275));}
 function hc(h){return new Array(3).fill().map((x,i)=>sh(ch(h+(510*i))))}
 function hu(h){return "rgb("+hc(h).join(",")+")"}
 
-function ui(i,v,a){x(a)?e(i).setAttribute(a,v):e(i).innerHTML=v}
-function ut(i){return e(i).innerText}
+function ui(i,v,a){return x(v)?x(a)?e(i).setAttribute(a,v):e(i).innerHTML=v:e(i).innerText}
 function uh(d,ids,t){
   if (typeof ids=='string') {ids=[ids]}
   var h=d.getTime()/t; var l=ids.length;
   ids.forEach((n,i) => cc(n,hu(h-(i/l*1530))))}
 
-function b(x){document.body.style.background=x}
+function b(x){document.body.style.background="#000 url('bg/"+x.toString().padStart(2,'0')+".webp') center / cover no-repeat"}
 function ri(x){return Math.trunc(Math.random()*x)+1}
-function rb(x){b("#000 url('bg/"+ri(x).toString().padStart(2,'0')+".webp') center / cover no-repeat")}
+function rb(x){
+  d.bl=d.bc; while(d.bc==d.bl){d.bc=ri(x)}
+  d.bs.add(d.bc); b(d.bc)}
 
 function main() {uh(new Date(),['pp','m4a','m4b','m4c','m4d'],137);requestAnimationFrame(main)}
 
 function rs() {
   var w = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
   var h = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
-  var pw = 700; var ph = 800;
-  document.body.style.zoom = Math.min(w/pw,h/ph);
-}
+  document.body.style.zoom = Math.min(w/d.pw,h/d.ph)}
+
+var d = {pw:700,ph:800,ba:6,bb:18,bc:0,bl:0,bs:new Set()}
 
 ui('year',new Date().getFullYear());
-ui('em',"mailto:"+['me',ut('h')].join('@'),'href');
+ui('em',"mailto:"+['me',ui('h')].join('@'),'href');
 rs();k('l');main();
-if(!ut('h').startsWith('404')){rb(6)}
+if(!ui('h').startsWith('404')){rb(d.ba)}
