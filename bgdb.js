9a3e8a6b3a8ca4f9879917e23255a12abb3ffa69
Josh <74288129+jfslee@users.noreply.github.com>
2026-03-13 10:47:13 +1100

    make bgdb easier to read

--- bgdb.js
+++ bgdb.js
@@ -1,122 +1,32 @@
-db={
-1:{
-  name:"flame",
-  loc:"East Point, Darwin",
-  pr:12},
-2:{
-  name:"daybreak",
-  loc:"Neagles Rock, Clare Valley",
-  pr:9},
-3:{
-  name:"buildup",
-  loc:"Casuarina Beach, Darwin",
-  pr:10},
-4:{
-  name:"ancestral",
-  loc:"Sungai Pinji, Perak, Malaysia",
-  pr:7},
-5:{
-  name:"fade",
-  loc:"Dripstone Cliffs, Darwin",
-  pr:11},
-6:{
-  name:"wander",
-  loc:"Buffalo Creek Reserve, Sydney",
-  pr:6},
-7:{
-  name:"breeze",
-  loc:"Market Street, Sydney",
-  pr:8},
-8:{
-  name:"icon",
-  loc:"Sydney Opera House",
-  pr:3},
-9:{
-  name:"remembrance",
-  loc:"Bukit Kledang, Menglembu",
-  pr:6},
-10:{
-  name:"dreaming",
-  loc:"Ulur&#x0331;u-Kata Tjut&#x0331;a National Park",
-  pr:2},
-11:{
-  name:"return",
-  loc:"Sydney Airport Gateway",
-  pr:1},
-12:{
-  name:"endless",
-  loc:"Barkly Tablelands, outback NT",
-  pr:4},
-13:{
-  name:"commodity",
-  loc:"Kuala Lumpur, Malaysia",
-  pr:5},
-14:{
-  name:"influence",
-  loc:"Bundilla Beach, Darwin",
-  pr:3},
-15:{
-  name:"attractor",
-  loc:"Dorrigo National Park",
-  pr:2},
-16:{
-  name:"stand",
-  loc:"Sydney Town Hall",
-  pr:1},
-17:{
-  name:"meeting",
-  loc:"Nightcliff, Darwin",
-  pr:1},
-18:{
-  name:"sunday",
-  loc:"Bronte Beach, Sydney",
-  pr:6},
-19:{
-  name:"molten",
-  loc:"The Star, Brisbane",
-  pr:2},
-20:{
-  name:"weaver",
-  loc:"George Brown Botanic Gardens",
-  pr:1},
-21:{
-  name:"society",
-  loc:"International Towers Barangaroo",
-  pr:4},
-22:{
-  name:"paper",
-  loc:"Ipoh Old Town, Malaysia",
-  pr:3},
-23:{
-  name:"assured",
-  loc:"Australian National Maritime Museum",
-  pr:2},
-24:{
-  name:"snack",
-  loc:"Territory Wildlife Park, Darwin",
-  pr:2},
-25:{
-  name:"fetch",
-  loc:"Gallows Beach, Coffs Harbour",
-  pr:1},
-26:{
-  name:"artery",
-  loc:"Main North Road, SA",
-  pr:1},
-27:{
-  name:"auspicious",
-  loc:"Queen Victoria Building",
-  pr:1},
-28:{
-  name:"shell",
-  loc:"Tumbalong Park, Darling Harbour",
-  pr:1},
-29:{
-  name:"nice",
-  loc:"Tattersalls Lane, Melbourne",
-  pr:1},
-30:{
-  name:"stopover",
-  loc:"Renner Springs, outback NT",
-  pr:2}
+db=
+{1:{name:"flame",loc:"East Point, Darwin",pr:12}
+,2:{name:"daybreak",loc:"Neagles Rock, Clare Valley",pr:9}
+,3:{name:"buildup",loc:"Casuarina Beach, Darwin",pr:10}
+,4:{name:"ancestral",loc:"Sungai Pinji, Perak, Malaysia",pr:7}
+,5:{name:"fade",loc:"Dripstone Cliffs, Darwin",pr:11}
+,6:{name:"wander",loc:"Buffalo Creek Reserve, Sydney",pr:6}
+,7:{name:"breeze",loc:"Market Street, Sydney",pr:8}
+,8:{name:"icon",loc:"Sydney Opera House",pr:3}
+,9:{name:"remembrance",loc:"Bukit Kledang, Menglembu",pr:6}
+,10:{name:"dreaming",loc:"Ulur&#x0331;u-Kata Tjut&#x0331;a National Park",pr:2}
+,11:{name:"return",loc:"Sydney Airport Gateway",pr:1}
+,12:{name:"endless",loc:"Barkly Tablelands, outback NT",pr:4}
+,13:{name:"commodity",loc:"Kuala Lumpur, Malaysia",pr:5}
+,14:{name:"influence",loc:"Bundilla Beach, Darwin",pr:3}
+,15:{name:"attractor",loc:"Dorrigo National Park",pr:2}
+,16:{name:"stand",loc:"Sydney Town Hall",pr:1}
+,17:{name:"meeting",loc:"Nightcliff, Darwin",pr:1}
+,18:{name:"sunday",loc:"Bronte Beach, Sydney",pr:6}
+,19:{name:"molten",loc:"The Star, Brisbane",pr:2}
+,20:{name:"weaver",loc:"George Brown Botanic Gardens",pr:1}
+,21:{name:"society",loc:"International Towers Barangaroo",pr:4}
+,22:{name:"paper",loc:"Ipoh Old Town, Malaysia",pr:3}
+,23:{name:"assured",loc:"Australian National Maritime Museum",pr:2}
+,24:{name:"snack",loc:"Territory Wildlife Park, Darwin",pr:2}
+,25:{name:"fetch",loc:"Gallows Beach, Coffs Harbour",pr:1}
+,26:{name:"artery",loc:"Main North Road, SA",pr:1}
+,27:{name:"auspicious",loc:"Queen Victoria Building",pr:1}
+,28:{name:"shell",loc:"Tumbalong Park, Darling Harbour",pr:1}
+,29:{name:"nice",loc:"Tattersalls Lane, Melbourne",pr:1}
+,30:{name:"stopover",loc:"Renner Springs, outback NT",pr:2}
 }
