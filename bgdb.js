823390686d486ba21caa7301e7363c38ee6efd37
Josh <74288129+jfslee@users.noreply.github.com>
2026-04-05 10:39:18 +1000

    add bg 38

--- bgdb.js
+++ bgdb.js
@@ -35,5 +35,6 @@ db=
 ,34:{name:"gated",loc:"Blackwattle Bay, Sydney",pr:1}
 ,35:{name:"attention",loc:"River Foreshore Reserve, Parramatta",pr:1}
 ,36:{name:"hi-vis",loc:"Kuala Lumpur, Malaysia",pr:1}
-,37:{name:"awake",loc:"Rose Bay Marina, Sydney",pr:2}
+,37:{name:"restless",loc:"Rose Bay Marina, Sydney",pr:2}
+,38:{name:"empty",loc:"Angel Place, Sydney",pr:1}
 }
